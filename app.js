1e3dcb7aaad725442656c9a2bf2eb33a02cae1d4
ultimate-knight <maaz2607@gmail.com>
2024-08-16 16:56:04 +0530

    hash router changes

---
+++ app.js
@@ -0,0 +1,59 @@
+const apiKey = "e9a4d0c515b00f63d8c4f2f101977df7";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "/images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "/images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "/images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "/images/drizzles.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "/images/mist.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkWeather(searchBox.value);
+  window.location.hash = searchBox.value;
+});
+
+searchFromHash();
+
+window.addEventListener("hashchange", () => {
+  searchFromHash();
+});
+
+function searchFromHash() {
+  if (window.location.hash !== "") {
+    searchBox.value = window.location.hash.replace("#", "");
+    checkWeather(searchBox.value);
+  }
+}
